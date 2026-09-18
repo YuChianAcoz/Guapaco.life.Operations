9a503bb2adf2f532961022f7a301492d7756ed89
YuChianAcoz <143982220+YuChianAcoz@users.noreply.github.com>
2026-09-18 14:27:34 +0800

    Update printer-config.js

--- printer-config.js
+++ printer-config.js
@@ -39,7 +39,7 @@ const PRINTER_CONFIG = {
       deliveryNo: 6,
     },
     fonts: {
-      date: { font: "Meiryo", size: 13 },
+      date: { font: "Meiryo", size: 12.5 },
       documentType: { font: "新細明體", size: 19 },
       deliveryNo: { font: "Meiryo", size: 11.2 },
       tripNo: { font: "Meiryo", size: 12 },
